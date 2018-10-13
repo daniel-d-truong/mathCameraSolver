23f85d3afc901b33e6d174d734f32fb2a708a141
wacheemdue <j1do@ucsd.edu>
2018-10-13 04:45:21 -0700

    Initialize android app and can take photos!

---
+++ app/src/main/java/com/sdhaxwoo/mathsolvercamera/MainActivity.java
@@ -0,0 +1,38 @@
+package com.sdhaxwoo.mathsolvercamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCamera = (Button) findViewById (R.id.btnCamera);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    protected void onActivity(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
